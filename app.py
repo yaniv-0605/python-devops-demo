1f00a36e1b0a998986fa49fcd5ecd37bf2478191
Dev-vin-bit <vinayreddygari89gmail.com>
2026-02-24 13:46:03 +0530

    fixed ci pipeline

--- app.py
+++ app.py
@@ -5,7 +5,5 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return {"message": "My First python DevOps Project"}
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-
+    app.run(host="0.0.0.0" , port=5000)
