7cb4d14c8278ce6e6c5062a5c181ef03e9ec7c71
Dev-vin-bit <vinayreddygari89gmail.com>
2026-02-24 13:36:13 +0530

    Fixed ci Pipeline

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app = Flask(__name__)
 def home():
     return {"message": "My First python DevOps Project"}
 
-app.run(host="0.0.0.0", port=5000)
 if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
