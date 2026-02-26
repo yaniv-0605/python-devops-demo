3eae17fcbe3aaad3560e3e7d6a8cb00bfb63e881
Dev-vin-bit <vinayreddygari89gmail.com>
2026-02-26 20:16:52 +0530

    updated python file

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "My First python DevOps Project & this is the edited version"}
+    return {"message": "My First python DevOps Project & this is the edited versiono of file"}
 if __name__ == "__main__":
     app.run(host="0.0.0.0" , port=5000)
